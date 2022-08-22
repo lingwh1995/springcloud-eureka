b4a9bc00433838c8f0b956e17a5cb37a848c239d
lingwh <LRwq942133>
2022-08-22 12:41:36 +0800

    增加创建目录树组件的脚本

---
+++ createTreeMD.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#为当前目录中的所有子模块创建tree.md文件
+function createTreeMDForChildDir() {
+    for item in `ls $1`
+    do
+    fileName=$item
+    if [ -d $fileName ]
+    then
+	#删除旧的tree.md文件
+	rm -rf $fileName/tree.md
+	#创建新的tree.md文件
+	tree $fileName > $fileName/tree.md
+    fi
+    done
+}
+#为当前目录创建tree.md文件
+function createTreeMDForCurrentDir(){
+    #删除旧的tree.md文件
+    rm -rf tree.md
+    #创建新的tree.md文件
+    tree > tree.md
+}
+#替换当前目录下tree.md第一行.为当为文件夹名称
+function replaceDotToBaseDirName(){
+     #替换当前文件夹下tree.md文件的第一行的.为当前文件夹名称	
+     sed -i "1,/./c\springcloud-eureka" tree.md
+}
+createTreeMDForChildDir
+createTreeMDForCurrentDir
+replaceDotToBaseDirName
