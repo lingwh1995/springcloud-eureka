d092338a51f77848cb23e5cbdb9ae34d6b079f05
lingwh <LRwq942133>
2022-08-23 01:29:14 +0800

    增加创建目录树组件的脚本

--- createTreeMD.sh
+++ createTreeMD.sh
@@ -15,7 +15,7 @@ function afterCreateTreeMD() {
 }
 #为当前目录中的所有子模块创建tree.md文件
 function createTreeMDForChildDir() {
-    for item in `ls -I note -I createTreeMD.sh`
+    for item in `ls $1`
     do
     fileName=$item
     if [ -d $fileName ]
@@ -32,7 +32,7 @@ function createTreeMDForCurrentDir(){
     #删除旧的tree.md文件
     rm -rf tree.md
     #创建新的tree.md文件
-    tree > tree.md
+    tree -I 'note|tree.md|createTreeMD.sh' > tree.md
 }
 #替换当前目录下tree.md第一行.为当为文件夹名称
 function replaceDotToBaseDirName(){
