290f72500f9bcf6186c9604d33615108612ecb34
lingwh <LRwq942133>
2022-08-23 15:16:27 +0800

    更新创建目录树组件的脚本

--- createTreeMD.sh
+++ createTreeMD.sh
@@ -18,15 +18,13 @@ function createTreeMDForChildDir() {
     then
         #创建新的tree.md文件
         tree $fileName -I 'tree.md' --dirsfirst > $fileName/tree.md
-        #替换`为•
-        sed -i 's/`/•/g' $fileName/tree.md
         #获取结束删除的行数
         END_LINE_NUMBER=`cat $fileName/tree.md | wc -l`
-        LAST_LINE_STR=`cat $fileName/tree.md | tail -3| head -n 1`
         #获取开始删除的行数
         START_LINE_NUMBER=$[$END_LINE_NUMBER-1]
         sed -i ''"$START_LINE_NUMBER"','"$END_LINE_NUMBER"'d' $fileName/tree.md
-        sed -i '$i '"$START_LINE_NUMBER"'' $fileName/tree.md
+        #替换`为•
+        sed -i 's/`/•/g' $fileName/tree.md
     fi
     done
 }
@@ -43,15 +41,13 @@ function createTreeMDForCurrentDir(){
 function formatTreeMD(){
     #替换当前文件夹下tree.md文件的第一行的.为当前文件夹名称
     sed -i '1,/./c\'"$1"'' tree.md
-    #替换`为•
-    sed -i 's/`/•/g' tree.md
     #获取结束删除的行数
     END_LINE_NUMBER=`cat tree.md | wc -l`
-    LAST_LINE_STR=`cat tree.md | tail -3| head -n 1`
     #获取开始删除的行数
     START_LINE_NUMBER=$[$END_LINE_NUMBER-2]
     sed -i ''"$START_LINE_NUMBER"','"$END_LINE_NUMBER"'d' tree.md
-    sed -i '$i '"$START_LINE_NUMBER"'' tree.md
+    #替换`为•
+    sed -i 's/`/•/g' tree.md
 }
 
 
