8c398a488d15cb929406a82b05bb3e1e2386ee89
lingwh <LRwq942133>
2022-08-23 11:24:06 +0800

    更新创建目录树组件的脚本

--- createTreeMD.sh
+++ createTreeMD.sh
@@ -20,9 +20,12 @@ function createTreeMDForChildDir() {
         tree $fileName -I 'tree.md' --dirsfirst > $fileName/tree.md
         #替换`为•
         sed -i 's/`/•/g' $fileName/tree.md
+        #删除最后三行
         #获取结束删除的行数
-        LAST_LINE_NUMBER=`cat $fileName/tree.md | wc -l`
-        cat $fileName/tree.md | head -n $LAST_LINE_NUMBER > $fileName/tree.md
+        END_LINE_NUMBER=`cat $fileName/tree.md | wc -l`
+        #获取开始删除的行数
+        START_LINE_NUMBER=$[$END_LINE_NUMBER-1]
+        sed -i ''"$START_LINE_NUMBER"','"$END_LINE_NUMBER"'d' $fileName/tree.md
     fi
     done
 }
@@ -43,8 +46,10 @@ function formatTreeMD(){
     sed -i 's/`/•/g' tree.md
     #删除最后三行
     #获取结束删除的行数
-    LAST_LINE_NUMBER=`cat tree.md | wc -l`
-    cat tree.md | head -n $LAST_LINE_NUMBER > tree.md
+    END_LINE_NUMBER=`cat tree.md | wc -l`
+    #获取开始删除的行数
+    START_LINE_NUMBER=$[$END_LINE_NUMBER-1]
+    sed -i ''"$START_LINE_NUMBER"','"$END_LINE_NUMBER"'d' tree.md
 }
 
 
