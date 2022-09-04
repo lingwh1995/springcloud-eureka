c2dce79d36aaf648014c0962ce1033e66b53e5af
lingwh <LRwq942133>
2022-09-04 19:00:08 +0800

    更新内容

--- springcloud-ci-k8s80/src/main/java/org/openatom/springcloud/CiK8s80.java
+++ springcloud-ci-k8s80/src/main/java/org/openatom/springcloud/CiK8s80.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CiK8s80 {
+
     public static void main(String[] args) {
         SpringApplication.run(CiK8s80.class, args);
     }
